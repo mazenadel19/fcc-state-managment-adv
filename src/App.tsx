d2c0879c2dcdf48241423072f7f47903dc6f460a
Mazen Emam <mazenadel19@gmail.com>
2022-12-02 19:13:52 +0200

    usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useMemo, useState } from "react";
+import "./App.css";
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function App() {
+  const sum = useMemo(
+    () =>
+      numbers.reduce((acc, curr) => {
+        return acc + curr;
+      }, 0),
+    [numbers]
+  );
+
+  const [names] = useState(["john", "jane", "joe"]);
+  const sortedNames = useMemo(() => [...names].sort(), [names]);
 
   return (
-    <div className="App">hi</div>
-  )
+    <div className="App">
+      <div>{sum}</div>
+      <div>{names.join(', ')}</div>
+      <div>{sortedNames.join(', ')}</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
