07c5d7919c1cec383b6e38187795a8db4bd9eda9
Mazen Emam <mazenadel19@gmail.com>
2022-12-03 00:59:23 +0200

    wrong

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { useHttp } from './useHttp';
+import { Person } from './types';
+import './App.css';
 
 function App() {
+  // const [selectedId, setSelectedId] = useState<number>(1);
+  // const people = useHttp({ url: 'https://jsonplaceholder.typicode.com/users' });
+  const [selectedId, setSelectedId] = useState('');
+  const people = useHttp({ url: '/names.json' });
 
   return (
-    <div className="App">hi</div>
-  )
+    <div className="App">
+      {(people as Person[])?.map(({ id, name }) => (
+        <button key={id} onClick={() => setSelectedId(name)}>
+          {name}
+        </button>
+      ))}
+      <br />
+      <InnerComponent selectedId={selectedId} />
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+const InnerComponent = ({ selectedId }: { selectedId: number | string }) => {
+  const person = useHttp({ url: `/${selectedId}.json` });
+
+  return <div>{(person as Person)?.username}</div>;
+};
