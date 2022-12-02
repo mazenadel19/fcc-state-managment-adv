e47e31f7fc0ea0a85f90e751b66df17c38944eb4
Mazen Emam <mazenadel19@gmail.com>
2022-12-02 17:28:43 +0200

    basic useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+function Counter() {
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">hi</div>
-  )
+    <div>
+      <button onClick={() => setCount((p) => p + 1)}>{count}</button>
+    </div>
+  );
 }
 
-export default App
+function NameList() {
+  const [nameList, setNameList] = useState(["amy", "bebop", "cathy"]);
+  const [name, setName] = useState("");
+
+  const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setNameList((p) => [...p, name]);
+    setName('')
+  };
+  return (
+    <div>
+      {nameList.map((name,index) => (
+        <p key={index}>{name}</p>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <button>submit</button>
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <NameList />
+    </div>
+  );
+}
+
+export default App;
