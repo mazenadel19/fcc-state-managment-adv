5925d689c1de0e9777a4848e5dd2aeca71c453c1
Mazen Emam <mazenadel19@gmail.com>
2022-12-03 01:23:07 +0200

    clean

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,7 @@
-import { useState } from 'react';
-import { useHttp } from './useHttp';
-import { Person } from './types';
 import './App.css';
 
 function App() {
-  const [data, setData] = useState<Person | undefined>();
-  const people = useHttp({ url: '/names.json' });
-
-  const onSelectName = (name: string) => {
-    if (name) {
-      fetch(`/${name}.json`)
-        .then((res) => res.json())
-        .then((data) => setData(data));
-    }
-  };
-
-  return (
-    <div className="App">
-      {(people as Person[])?.map(({ id, name }) => (
-        <button key={id} onClick={() => onSelectName(name)}>
-          {name}
-        </button>
-      ))}
-      <br />
-      <InnerComponent selected={data} />
-    </div>
-  );
+  return <div className="App">hi</div>;
 }
 
 export default App;
-
-const InnerComponent = ({ selected }: { selected: Person | undefined }) => {
-  return selected ? <div>{(selected as Person).username}</div> : null;
-};
