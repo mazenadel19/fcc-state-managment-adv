8f2c9888e70e406d074f995fdd6b8d693e32b329
Mazen Emam <mazenadel19@gmail.com>
2022-12-03 15:47:02 +0200

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 
 function App() {
-  return <div className="App">hi</div>;
+  const [people, setPeople] = useState<[] | { id: number; name: string }[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const data = inputRef.current?.value;
+    e.preventDefault();
+    data && setPeople((prev) => [...prev, { id: prev.length, name: data }]);
+    inputRef.current?.value && (inputRef.current.value = '');
+  };
+
+  console.log(people);
+
+  return (
+    <div className="App">
+      {people.map((person) => (
+        <h1 key={person.id}>{person.name}</h1>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="test">
+          test <br />
+          <input type="text" ref={inputRef} id="test" />
+        </label>
+        <br />
+
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
